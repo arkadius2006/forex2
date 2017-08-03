61f980c8220bfa3b02dcb5d356336c1b6094ed5c
Arkadiy Seletskiy <arkadius2006@gmail.com>
2017-08-03 10:31:54 +0300

    about component

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-about-component',
+  styleUrls: ['about.component.css'],
+  templateUrl: 'about.component.html',
+})
+
+export class AboutComponent {
+}
