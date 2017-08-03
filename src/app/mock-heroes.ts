17e33bef7996dc2defe9e0f867981949c931ac96
Arkadiy Seletskiy <arkadius2006@gmail.com>
2017-08-03 10:40:32 +0300

    app cleanup

--- src/app/mock-heroes.ts
+++
@@ -1,8 +0,0 @@
-import {Hero} from './hero';
-
-export const HEROES: Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'}
-];
